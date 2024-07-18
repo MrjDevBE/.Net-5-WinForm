718a0143a37151460ea5a7db1f58a3af58d74722
MrjDevBE <132418269+MrjDevBE@users.noreply.github.com>
2024-07-18 19:18:15 +0800

    Commit
    Commit

---
+++ TestApp/TestApp/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTimer = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(381, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 211);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(776, 227);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Status: Calculation is not started.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(407, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "100";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(274, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generate Files";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(407, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(381, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Show current result";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(407, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(381, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Show result";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1. Enter files count and click \'Generate Files\':";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "2. Click \'Execute\' to start calculation:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Status message:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(238, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "3. Click \'Show current result\' to see:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "4. Click \'Show result\' to see the result:";
+            // 
+            // textBoxTimer
+            // 
+            this.textBoxTimer.Location = new System.Drawing.Point(610, 444);
+            this.textBoxTimer.Name = "textBoxTimer";
+            this.textBoxTimer.Size = new System.Drawing.Size(178, 27);
+            this.textBoxTimer.TabIndex = 12;
+            this.textBoxTimer.Text = "Intermediate Total";
+            this.textBoxTimer.TextChanged += new System.EventHandler(this.textBoxTimer_TextChanged);
+            // 
+            // timer1
+            // 
+            //this.timer1.Interval = 1000;
+            //this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.textBoxTimer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "File content calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTimer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ TestApp/TestApp/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        private string baseDirectory = ConfigurationManager.AppSettings["Directory1"];
+        private string folderName = ConfigurationManager.AppSettings["FolderName"];
+        private string folderCount = ConfigurationManager.AppSettings["FolderCount"];
+        private string faxNum = ConfigurationManager.AppSettings["MaxNum"];
+
+        private ConcurrentBag<int> _results;
+        private ConcurrentBag<string> _fileNames;
+        private int _total;
+        private bool _isProcessing;
+        private int _remainingTime = 60; // 60 seconds countdown
+        private System.Diagnostics.Stopwatch stopwatch;
+
+        private object _lock = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeVariables();
+            //InitializeTimer();
+        }
+
+        private void InitializeVariables()
+        {
+            _results = new ConcurrentBag<int>();
+            _fileNames = new ConcurrentBag<string>();
+            _total = 0;
+            _isProcessing = false;
+            stopwatch = new System.Diagnostics.Stopwatch();
+        }
+
+        //private void InitializeTimer()
+        //{
+        //    timer1.Interval = 1000; // Set the timer interval to 1 second
+        //    timer1.Tick += timer1_Tick;
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numberOfFiles;
+            if (!int.TryParse(textBox1.Text, out numberOfFiles) || numberOfFiles <= 0)
+            {
+                MessageBox.Show("Please enter a valid number of files.");
+                return;
+            }
+
+            string directoryPath = Path.Combine(baseDirectory, folderName);
+
+            try
+            {
+                if (!Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath);
+
+                Random random = new Random();
+
+                for (int i = 0; i < numberOfFiles; i++)
+                {
+                    string fileName = random.Next(Convert.ToInt32(folderCount)).ToString(); // Generate unique file names
+                    string filePath = Path.Combine(directoryPath, fileName + ".txt");
+                    int content = random.Next(Convert.ToInt32(faxNum)); // More varied content
+                    File.WriteAllText(filePath, content.ToString());
+                }
+
+                MessageBox.Show($"{numberOfFiles} files generated in 'Files' folder.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating files: {ex.Message}");
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (_isProcessing) return;
+            _isProcessing = true;
+
+            _results = new ConcurrentBag<int>();
+            _fileNames = new ConcurrentBag<string>();
+            _total = 0;
+            _remainingTime = 60; // Reset countdown timer
+
+            stopwatch.Restart(); // Start the stopwatch
+
+            try
+            {
+                await Task.Run(() => ProcessFilesAsync());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error processing files: {ex.Message}");
+            }
+            finally
+            {
+                stopwatch.Stop(); // Stop the stopwatch
+
+                // Format elapsed time including milliseconds
+                string elapsedTime = $"{stopwatch.Elapsed:mm\\:ss\\.fff}";
+
+                MessageBox.Show($"Calculation complete. Elapsed Time: {elapsedTime}");
+
+                // Update textBoxTimer with elapsed time including milliseconds
+                textBoxTimer.Text = $"Elapsed Time: {elapsedTime}";
+
+                _isProcessing = false;
+            }
+        }
+
+        private void ProcessFilesAsync()
+        {
+            string directoryPath = Path.GetFullPath(baseDirectory);
+
+            if (!Directory.Exists(directoryPath))
+                throw new DirectoryNotFoundException($"Files directory '{directoryPath}' does not exist.");
+
+            string[] files = Directory.GetFiles(directoryPath);
+
+            Parallel.ForEach(files, file => ProcessFile(file));
+        }
+
+        private void ProcessFile(string filePath)
+        {
+            string content = File.ReadAllText(filePath);
+            if (int.TryParse(content, out int number))
+            {
+                _results.Add(number);
+                _fileNames.Add(Path.GetFileNameWithoutExtension(filePath));
+                lock (_lock)
+                {
+                    _total += number;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShowResults(intermediate: true);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ShowResults(intermediate: false);
+        }
+
+        private void ShowResults(bool intermediate)
+        {
+            string sortedFileNames = string.Join(";", _fileNames.OrderBy(name => int.Parse(name)));
+            int totalSum = _results.Sum();
+            textBox2.Text = $"{sortedFileNames}\r\nTotal: {totalSum}";
+        }
+
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    if (_remainingTime > 0)
+        //    {
+        //        _remainingTime--;
+        //        textBoxTimer.Text = $"Intermediate Total: {_total} | Time left: {_remainingTime}s";
+        //    }
+        //    else
+        //    {
+        //        timer1.Stop();
+        //    }
+        //}
+        private void textBoxTimer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
